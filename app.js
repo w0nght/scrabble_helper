a64434860fd122d9a8a6829a7908602072499c89
w0nght <wwongheitung@gmail.com>
2025-07-04 15:49:08 +1000

    Removed yml job

--- app.js
+++ app.js
@@ -165,11 +165,12 @@ fetch('https://api.github.com/repos/w0nght/scrabble_helper/tags')
   .then(res => res.json())
   .then(tags => {
     if (tags.length > 0) {
-      document.getElementById('version').textContent = `Version: ${tags[0].name} | &copy; 2025 Joey Wong`;
+      document.getElementById('version').innerHTML = `Version: ${tags[0].name} | &copy; 2025 Joey Wong`;
     } else {
-      document.getElementById('version').textContent = "Version: dev | &copy; 2025 Joey Wong`";
+      document.getElementById('version').innerHTML = "Version: dev | &copy; 2025 Joey Wong`";
     }
   })
   .catch(() => {
-    document.getElementById('version').textContent = "Version: offline | &copy; 2025 Joey Wong";
+    console.error('Error fetching version:', error);
+    document.getElementById('version').innerHTML = "Version: offline | &copy; 2025 Joey Wong";
   });
